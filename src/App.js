d43b1817612e6bb38f9fd250f94293d3b4ac9811
shayesteh <schaayesteh@gmail.com>
2023-07-10 13:01:07 +0330

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
+import './index.css'
+import Pad from './components/Pad';
+import audioClips from './audioClips'
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [volume, setVolume] = useState(1);
+  const [display, setDisplay] = useState("Tap The Keys!");
+
+  const handlePadClick = (padId) => {
+    setDisplay(padId);
+  }
+
+  const handleKeyPress = (event) => {
+    const pressedKey = event.key.toUpperCase();
+    const pad = audioClips.find((clip) => clip.keyTrigger === pressedKey);
+    if (pad) {
+      setDisplay(pad.id);
+    }
+  };
+
+  const handleVolumeChange = (event) => {
+    const newVolume = parseFloat(event.target.value);
+    setVolume(newVolume);
+    audioClips.forEach((clip) => {
+      clip.volume = newVolume;
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyPress);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyPress);
+    };
+  }, [volume]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='drum-machine' className='drum-machine-container'>
+
+      <div className="pad-bank">
+        {audioClips.map(clip => (
+          <div className="drum-pad" onClick={() => handlePadClick(clip.id)}>
+            <Pad
+              key={clip.id}
+              clip={clip}
+              volume={volume}
+            />
+          </div>
+        ))}
+      </div>
+
+      <div id='display'>
+        {display}
+      </div>
+      <div className='volume'>
+        <h6>Volume</h6>
+        <input
+          type='range'
+          step='0.01'
+          min='0'
+          max='1'
+          value={volume}
+          className='w-10 volume-range'
+          onChange={handleVolumeChange}
+        />
+      </div>
+
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/Pad.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+
+export default function Pad({ clip, volume }) {
+
+    const [active, setActive] = useState(false);
+
+    useEffect(() => {
+        document.addEventListener('keydown', handleKeyPress);
+        return () => { document.removeEventListener('keydown', handleKeyPress); }
+    }, [])
+
+    const handleKeyPress = (e) => {
+        if (e.keyCode === clip.keyCode) {
+            playSound();
+        }
+    };
+
+    const playSound = () => {
+        const audioTag = document.getElementById(clip.keyTrigger);
+        setActive(true);
+        setTimeout(() => setActive(false), 200);
+        audioTag.volume = volume;
+        audioTag.currentTime = 0;
+        audioTag.play();
+    };
+
+    return (
+        <div onClick={playSound} className={`btn btn-secondary w-90 h-90 p-4 m-1 ${active && 'btn-danger'} pad-btn`}>
+            <audio className="clip" id={clip.keyTrigger} src={clip.url} />
+            {clip.keyTrigger}
+        </div>
+    );
+}  
